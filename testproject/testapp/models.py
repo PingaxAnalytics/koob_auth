ac837ec0914e6c59b530f67fafd2d532836be7ea
vignesh <vignesh2066@gmail.com>
2016-12-18 01:28:47 +0530

    updated models parameters

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -1,28 +1,72 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class MyUser(AbstractUser):
-    websiteUrl = models.CharField(max_length=100,unique=True)
-    callbackUrl = models.CharField(max_length=100, blank=True, null=True)
-    temporaryCredentialsRequestUrl = models.CharField(blank=True, max_length=100)
-    adminAuthorizationUrl  = models.CharField(blank=True, max_length=10)
-    accessTokenRequestUrl = models.CharField(blank=True, max_length=10)
-    consumerKey = models.CharField(blank=True, max_length=10)
-    consumerSecret = models.CharField(blank=True, max_length=10)
-    access_token = models.CharField(blank=True, max_length=10)
-    refresh_token = models.CharField(blank=True, max_length=10)
+    PLAN_IDS = (
+        (1, 'Free'),
+        (0, 'Development'),
+        (2, 'Enterprise'),
+    )
 
+    ENABLE_IDS = (
+        (1, True),
+        (0, False),
+    )
 
-    REQUIRED_FIELDS = ['callbackUrl',
-                       'temporaryCredentialsRequestUrl',
-                       'adminAuthorizationUrl',
-                       'accessTokenRequestUrl',
-                       'consumerKey',
-                       'consumerSecret',
+    # Magento parameters
+    oauth_initiate_url = models.CharField(max_length=200, blank=True)
+    admin_authorize_url = models.CharField(max_length=200, blank=True)
+    access_token_url = models.CharField(max_length=200, blank=True)
+    api_url = models.CharField(max_length=200, blank=True)
+    consumer_key = models.CharField(max_length=100, blank=True)
+    consumer_secret = models.CharField(max_length=100, blank=True)
+
+    # Google Analytics parameters
+    access_token = models.CharField(max_length=100, blank=True)
+    refresh_token = models.CharField(max_length=100, blank=True )
+
+    # Koob prefered settings
+    enable_discount = models.BooleanField(choices=ENABLE_IDS,default=1, blank=True)
+    per_discount = models.FloatField(default=0)
+
+    # User's primary information
+    name = models.CharField(max_length=100, blank=True)
+    email = models.CharField(max_length=100, blank=True)
+    contact_no = models.CharField(max_length=100, blank=True)
+    website_url = models.CharField(max_length=100, blank=True, unique=True)
+    plan_id = models.IntegerField(choices=PLAN_IDS, blank=True, default=1)
+
+    created = models.DateTimeField(editable=False, null=True)
+    modified = models.DateTimeField(null=True)
+
+    REQUIRED_FIELDS = ['oauth_initiate_url',
+                       'admin_authorize_url',
+                       'access_token_url',
+                       'api_url',
+                       'consumer_key',
+                       'consumer_secret',
                        'access_token',
                        'refresh_token',
-                       'websiteUrl']
+                       'enable_discount',
+                       'per_discount',
+                       'name',
+                       'email',
+                       'contact_no',
+                       'website_url',
+                       'plan_id',
+                       'created',
+                       'modified']
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(MyUser, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'MyUser'
